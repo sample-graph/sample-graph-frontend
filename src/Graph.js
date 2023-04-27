b64990adf175413ea2b92d7c7cb9c11b8a1d0ca7
Robert Yin <bobertoyin@gmail.com>
2023-04-27 13:22:52 -0400

    move API resource handlers to inside useEffect

--- src/Graph.js
+++ src/Graph.js
@@ -15,37 +15,37 @@ export function LoadGraph() {
     const { positions, assign } = useLayoutCircular();
     const node_color = "#000000";
     const node_size = 7;
-    const response_handler = (response) => {
-        const graph = new MultiDirectedGraph();
-        const nodes = response.data.nodes;
-        const edges = response.data.edges;
-        if (edges.length > 0) {
-            for (var edge of edges) {
-                const src = nodes[edge[0]];
-                const dst = nodes[edge[1]];
-                if (!graph.hasNode(src.id)) {
-                    graph.addNode(src.id, {x: 0, y: 0, size: node_size, label: `${src.title} by ${src.artist_name}`, color: node_color});
-                }
-                if (!graph.hasNode(dst.id)) {
-                    graph.addNode(dst.id, {x: 0, y: 0, size: node_size, label: `${dst.title} by ${dst.artist_name}`, color: node_color});
-                }
-                graph.addDirectedEdge(src.id, dst.id, {label: edge[2]});
-            }
-        } else {
-            for (var node of nodes) {
-                graph.addNode(node.id, {x: 0, y: 0, size: node_size, label: `${node.title} by ${node.artist_name}`, color: node_color});
-            }
-        }
-        loadGraph(graph);
-        assign();
-    };
-    const error_handler = (err) => {
-        toast.error(`Failed to get graph: ${err.message}`, {toastId: "graph-err"});
-    };
 
     React.useEffect(() => {
+        const response_handler = (response) => {
+            const graph = new MultiDirectedGraph();
+            const nodes = response.data.nodes;
+            const edges = response.data.edges;
+            if (edges.length > 0) {
+                for (var edge of edges) {
+                    const src = nodes[edge[0]];
+                    const dst = nodes[edge[1]];
+                    if (!graph.hasNode(src.id)) {
+                        graph.addNode(src.id, {x: 0, y: 0, size: node_size, label: `${src.title} by ${src.artist_name}`, color: node_color});
+                    }
+                    if (!graph.hasNode(dst.id)) {
+                        graph.addNode(dst.id, {x: 0, y: 0, size: node_size, label: `${dst.title} by ${dst.artist_name}`, color: node_color});
+                    }
+                    graph.addDirectedEdge(src.id, dst.id, {label: edge[2]});
+                }
+            } else {
+                for (var node of nodes) {
+                    graph.addNode(node.id, {x: 0, y: 0, size: node_size, label: `${node.title} by ${node.artist_name}`, color: node_color});
+                }
+            }
+            loadGraph(graph);
+            assign();
+        };
+        const error_handler = (err) => {
+            toast.error(`Failed to get graph: ${err.message}`, {toastId: "graph-err"});
+        };
         getResource(`graph/${id}`, response_handler, error_handler);
-    }, [id, response_handler, error_handler, loadGraph, positions, assign, node_color, node_size]);
+    }, [id, loadGraph, positions, assign, node_color, node_size]);
 
     return null;
 }
