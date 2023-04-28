fa2fd058fe6bed463cef62eae551bc505afb7f73
Robert Yin <bobertoyin@gmail.com>
2023-04-28 00:37:25 -0400

    improve graph presentation

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import { Block, Footer } from "react-bulma-components";
 
 export function AppFooter() {
     return (
-        <Footer textAlign="center" backgroundColor="white" paddingless={true}>
+        <Footer textAlign="center" backgroundColor="white" p={6}>
             <Block>
                 <p>
                     Created by <a href="https://bobertoyin.com" target="_blank" rel="noreferrer">Robert Yin</a>.
--- src/Graph.js
+++ src/Graph.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import MultiDirectedGraph from "graphology";
 import { SigmaContainer, useLoadGraph, ControlsContainer, ZoomControl, SearchControl, FullScreenControl } from "@react-sigma/core";
-import { useLayoutCircular } from "@react-sigma/layout-circular";
+import { useLayoutForceAtlas2 } from "@react-sigma/layout-forceatlas2";
+import { random } from "graphology-layout";
 import { Message } from "react-bulma-components";
 import { toast } from "react-toastify";
 import { ScaleLoader } from "react-spinners";
@@ -10,12 +11,22 @@ import "@react-sigma/core/lib/react-sigma.min.css";
 
 import { getResource } from "./utils";
 
+function addNode(graph, node, node_size, node_color) {
+    const label = `${node.song.title} by ${node.song.artist_name}`;
+    graph.addNode(node.song.id, {x: 0, y: 0, size: node_size, color: node_color, degree: node.degree, label: label});
+}
+
+function deSnakeCase(str) {
+    return str.replace("_", " ");
+}
+
 export function LoadGraph(props) {
     const { id } = useParams();
     const loadGraph = useLoadGraph();
-    const { positions, assign } = useLayoutCircular();
+    const { positions, assign } = useLayoutForceAtlas2();
     const node_color = "#000000";
-    const node_size = 7;
+    const node_size = 6;
+    const edge_size = 3;
 
     React.useEffect(() => {
         const response_handler = (response) => {
@@ -26,29 +37,31 @@ export function LoadGraph(props) {
                 for (var edge of edges) {
                     const src = nodes[edge[0]];
                     const dst = nodes[edge[1]];
-                    if (!graph.hasNode(src.id)) {
-                        graph.addNode(src.id, {x: 0, y: 0, size: node_size, label: `${src.title} by ${src.artist_name}`, color: node_color});
+                    if (!graph.hasNode(src.song.id)) {
+                        addNode(graph, src, node_size, node_color);
                     }
-                    if (!graph.hasNode(dst.id)) {
-                        graph.addNode(dst.id, {x: 0, y: 0, size: node_size, label: `${dst.title} by ${dst.artist_name}`, color: node_color});
+                    if (!graph.hasNode(dst.song.id)) {
+                        addNode(graph, dst, node_size, node_color);
                     }
-                    graph.addDirectedEdge(src.id, dst.id, {label: edge[2]});
+                    graph.addDirectedEdge(src.song.id, dst.song.id, {label: deSnakeCase(edge[2]), size: edge_size});
                 }
             } else {
                 for (var node of nodes) {
-                    graph.addNode(node.id, {x: 0, y: 0, size: node_size, label: `${node.title} by ${node.artist_name}`, color: node_color});
+                    addNode(graph, node, node_size, node_color);
                 }
             }
             loadGraph(graph);
+            random.assign(graph);
             assign();
             props.setLoading(false);
+
         };
         const error_handler = (err) => {
             toast.error(`Failed to get graph: ${err.message}`, {toastId: "graph-err"});
             props.setLoading(false);
         };
         getResource(`graph/${id}`, response_handler, error_handler);
-    }, [id, loadGraph, positions, assign, node_color, node_size, props]);
+    }, [id, loadGraph, positions, assign, node_color, node_size, edge_size, props]);
 
     return null;
 }
@@ -60,11 +73,20 @@ export function DisplayGraph() {
             <Message.Header radiusless={true}>
                 <a href="/">&larr; Return Home</a>
             </Message.Header>
-            <Message.Body style={{height: "70vh"}} paddingless={true} radiusless={true} backgroundColor="white">
+            <Message.Body style={{height: "65vh"}} paddingless={true} radiusless={true} backgroundColor="white">
                 <div style={{ height: "100%", width: "100%", display: loading ? "flex" : "none", alignContent: "center", alignItems: "center"}}>
                     <ScaleLoader radius={0} loading={loading} speedMultiplier={0.75} style={{margin: "auto"}}/>
                 </div>
-                <SigmaContainer style={{display: loading ? "none" : "flex"}}>
+                <SigmaContainer 
+                    style={{display: loading ? "none" : "flex"}} 
+                    settings={{
+                        defaultEdgeType: "arrow", 
+                        labelFont: "IBM Plex Mono, monospace", 
+                        renderEdgeLabels: true, 
+                        edgeLabelFont: "IBM Plex Mono, monospace", 
+                        edgeLabelColor: "black",
+                    }}
+                >
                     <LoadGraph setLoading={setLoading}/>
                     <ControlsContainer>
                         <SearchControl/>
