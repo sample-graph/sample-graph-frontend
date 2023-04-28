f1c6451413a9ac44da2005033fc8bb0efccd2800
Robert Yin <bobertoyin@gmail.com>
2023-04-27 21:31:13 -0400

    make more elements radiusless

--- src/Footer.js
+++ src/Footer.js
@@ -1,27 +1,14 @@
 import React from "react";
-import { Footer } from "react-bulma-components";
-import { toast } from "react-toastify";
-
-import { getResource } from "./utils";
+import { Block, Footer } from "react-bulma-components";
 
 export function AppFooter() {
-    const [version, setVersion] = React.useState(null);
-
-    React.useEffect(() => {
-        const response_handler = (response) => setVersion(response.data);
-        const error_handler = (err) => {
-            toast.error(`Failed to get API version: ${err.message}`, {toastId: "version-err"});
-            setVersion("?");
-        };
-        getResource("version", response_handler, error_handler);
-    }, []);
-
     return (
-        <Footer textAlign="center" backgroundColor="white">
-            <p>
-                Created by <a href="https://bobertoyin.com" target="_blank" rel="noreferrer">Robert Yin</a>.
-                Currently using <a href="https://github.com/bobertoyin/sample-graph-api" target="_blank" rel="noreferrer">SampleGraph API</a> v{version}.
-            </p>
+        <Footer textAlign="center" backgroundColor="white" paddingless={true}>
+            <Block>
+                <p>
+                    Created by <a href="https://bobertoyin.com" target="_blank" rel="noreferrer">Robert Yin</a>.
+                </p>
+            </Block>
         </Footer>
     );
 }
